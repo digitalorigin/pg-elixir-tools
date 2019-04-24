aa6868d950a2476c64478d621fc8368811cea203
David García Narbona <garcia.narbona@gmail.com>
2019-04-24 16:57:17 +0200

    Bump to 0.2.1 (#14)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ElixirTools.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @description "Tools used in pagantis for making developing easier"
 
   def project do
