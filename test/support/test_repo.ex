0895256bfc7036eb9f58bd426f94b40f9b1ac302
abeliaev <alex@digitalorigin.com>
2019-07-09 14:34:42 +0200

    add schema and related files

---
+++ test/support/test_repo.ex
@@ -0,0 +1,33 @@
+defmodule Support.TestRepo do
+  import Ecto.Changeset, only: [apply_changes: 1]
+
+  alias Support.TestSchema
+
+  @spec insert(any) :: {:ok, any}
+  def insert(changeset), do: {:ok, apply_changes(changeset)}
+
+  @spec insert!(any) :: any
+  def insert!(changeset), do: apply_changes(changeset)
+
+  @spec update(any) :: {:ok, any}
+  def update(changeset), do: {:ok, apply_changes(changeset)}
+  @spec update!(any) :: any
+  def update!(changeset), do: apply_changes(changeset)
+
+  @spec get(any, String.t()) :: TestSchema.t() | nil
+  def get(_, "existing" = id), do: %TestSchema{id: id}
+  def get(_, "non-existing"), do: nil
+
+  @spec get!(any, String.t()) :: TestSchema.t() | no_return()
+  def get!(_, "existing" = id), do: %TestSchema{id: id}
+  def get!(_, "non-existing" = id), do: raise(Ecto.NoResultsError, queryable: id)
+
+  @spec get_by!(any, Keyword.t()) :: TestSchema.t() | no_return()
+  def get_by!(_, id: "existing" = id), do: %TestSchema{id: id}
+  def get_by!(_, id: "non-existing" = id), do: raise(Ecto.NoResultsError, queryable: id)
+
+  @spec all(any, Keyword.t()) :: [TestSchema.t()]
+  def all(module, queryable \\ [])
+  def all(_, test_field_1: "dummy"), do: [%TestSchema{test_field_1: "dummy"}]
+  def all(_, _), do: [%TestSchema{}, %TestSchema{}]
+end
