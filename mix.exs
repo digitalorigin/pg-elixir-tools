3a667a3072e767285feae3a0d0848728eb7805f8
Hylke <12594000+hylkealons@users.noreply.github.com>
2019-08-21 11:18:56 +0200

    Bump version to 0.12.0 (#47)
    * Bump version to 0.12.0
    
    * Update CHANGELOG.md

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ElixirTools.MixProject do
   use Mix.Project
 
-  @version "0.11.1"
+  @version "0.12.0"
   @description "Tools used in pagantis for making developing easier"
 
   def project do
