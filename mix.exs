08f5dcb6e76dc3ac6327676c8901566e907a2320
Hylke <12594000+hylkealons@users.noreply.github.com>
2019-09-24 12:15:27 +0200

    Bump to 0.14.2 (#62)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ElixirTools.MixProject do
   use Mix.Project
 
-  @version "0.14.1"
+  @version "0.14.2"
   @description "Tools used in Pagantis for making developing easier"
 
   def project do
