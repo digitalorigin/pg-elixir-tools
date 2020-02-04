5445170dc00caedc7d7b7aa49685173cff8d6c15
Hylke <12594000+hylkealons@users.noreply.github.com>
2020-02-04 17:20:49 +0100

    elixir to 1.10 (#100)

--- config/config.exs
+++ config/config.exs
@@ -32,6 +32,7 @@ config :pagantis_elixir_tools, ElixirTools.Metrix,
   ]
 
 config :phoenix, :json_library, Jason
+
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
