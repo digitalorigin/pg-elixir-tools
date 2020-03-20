d1e8adf20144eba9b82cbdb72a6f07a6fd32eefb
Hylke <12594000+hylkealons@users.noreply.github.com>
2020-03-20 10:32:11 +0100

    Fix the issue with .all (#120)
    * Fix the issue with .all
    
    * Improve tests and documentation

--- lib/schema/schema_impl.ex
+++ lib/schema/schema_impl.ex
@@ -140,10 +140,10 @@ defmodule ElixirTools.Schema.SchemaImpl do
   def get_by!(module, queryable), do: module.repo.get_by!(module, queryable)
 
   @doc """
-  Fetches all records from the DB.
+  Gets all entities based on query. If no query is given, all entities are returned.
   """
   @spec all(ecto_schema, list) :: [t]
-  def all(module, queryable), do: module.repo.all(module, queryable)
+  def all(module, queryable), do: module.repo.all(queryable || module)
 
   @doc """
   Preloads a field on a struct.
