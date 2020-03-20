a97c6804c574747c5c9a467c046e165b404b3949
Kitton <alex@digitalorigin.com>
2020-03-20 11:30:21 +0100

    add tests

--- lib/credo/naming_check.ex
+++ lib/credo/naming_check.ex
@@ -1,10 +1,18 @@
 defmodule ElixirTools.Credo.NamingCheck do
-  def valid_filename?(filepath, module_name, _opts) do
-    filepath = String.replace(filepath, ["/controllers/", "/views/"], "/")
+  @moduledoc """
+  This mudule provides paths modification for typical Phoenix paths for controllers/views
+  To use it add `{CredoNaming.Check.Consistency.ModuleFilename, valid_filename_callback: &CredoNamingCheck.valid_filename?/3}`
+  to you .credo.exs
+  """
 
-    root_path = CredoNaming.Check.Consistency.ModuleFilename.root_path(filepath)
-    path = "#{Macro.underscore(module_name)}#{Path.extname(filepath)}"
-    filepaths = [Path.join([root_path, path])]
-    {filepath in filepaths, filepaths}
+  @parts_to_be_cleaned ["/controllers/", "/views/"]
+
+  def valid_filename?(filepath, module_name, opts) do
+    if(String.contains?(filepath, @parts_to_be_cleaned)) do
+      filepath = String.replace(filepath, @parts_to_be_cleaned, "/")
+      CredoNaming.Check.Consistency.ModuleFilename.valid_filename?(filepath, module_name, opts)
+    else
+      CredoNaming.Check.Consistency.ModuleFilename.valid_filename?(filepath, module_name, opts)
+    end
   end
 end
