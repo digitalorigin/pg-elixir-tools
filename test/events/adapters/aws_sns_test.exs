f72fbc4f76d35ff171028db4669ba870ee883c3a
dabeat <garcia.narbona@gmail.com>
2020-04-06 16:02:48 +0200

    event structure out of adpater

--- test/events/adapters/aws_sns_test.exs
+++ test/events/adapters/aws_sns_test.exs
@@ -4,7 +4,6 @@ defmodule ElixirTools.Events.Adapters.AwsSnsTest do
   import ExUnit.CaptureLog
 
   alias ElixirTools.Events.Adapters.AwsSns
-  alias ElixirTools.Events.Event
 
   defmodule FakeAwsSuccess do
     use ElixirTools.ContractImpl, module: ExAws
@@ -36,43 +35,17 @@ defmodule ElixirTools.Events.Adapters.AwsSnsTest do
     end
   end
 
-  defmodule FakeUuid do
-    use ElixirTools.ContractImpl, module: UUID
-
-    @impl true
-    def uuid5(uuid_seed_1, uuid_seed_2) do
-      send(self(), [:uuid5, uuid_seed_1, uuid_seed_2])
-      "55989ea4-d947-4f16-b8c8-e0e888facff4"
-    end
-  end
-
   setup do
-    valid_event = %Event{
-      name: "TEST_EVENT",
-      event_id_seed: "016c25fd-70e0-56fe-9d1a-56e80fa20b82"
+    valid_event = %{
+      id: "af7eae51-f47e-4ddb-a274-33037e17df6e",
+      action: "EVENT_ACTION",
+      group: "EVENT_GROUP",
+      occurred_at: "2020-01-02T00:00:00.000Z",
+      version: "1.0.0",
+      payload: %{data: "data"}
     }
 
     %{valid_event: valid_event}
-  end
-
-  test "uuid5 generation is called with a proper seed", context do
-    opts = [
-      aws_module: FakeAwsSuccess,
-      sns_module: FakeSnsSuccess,
-      uuid_module: FakeUuid,
-      topic: "topic",
-      group: "group",
-      default_region: "region"
-    ]
-
-    event = %{context.valid_event | event_id_seed_optional: "event_id_seed_optional"}
-    assert AwsSns.publish(event, opts) == :ok
-
-    assert_received [
-      :uuid5,
-      "016c25fd-70e0-56fe-9d1a-56e80fa20b82",
-      "TEST_EVENT-1.0.0-event_id_seed_optional"
-    ]
   end
 
   test "when event.occurred_at sent assert that it's used", context do
@@ -91,39 +64,7 @@ defmodule ElixirTools.Events.Adapters.AwsSnsTest do
 
     assert_received [
       :sns_success,
-      "{\"action\":\"TEST_EVENT\",\"group\":\"group\",\"id\":\"e8db4c36-f0b6-585a-a0df-0c388e87599b\",\"occurred_at\":\"2015-06-29T04:44:44Z\",\"payload\":{},\"version\":\"1.0.0\"}"
-    ]
-
-    assert_received [:ex_aws_success, _, _]
-  end
-
-  test "when event.occurred_at not sent assert that Timex.now is used", context do
-    defmodule FakeTimex do
-      use ElixirTools.ContractImpl, module: Timex
-
-      @impl true
-      def now() do
-        send(self(), :timex_now)
-        Timex.to_datetime({{2015, 6, 29}, {5, 55, 55}}, "Etc/UTC")
-      end
-    end
-
-    opts = [
-      aws_module: FakeAwsSuccess,
-      sns_module: FakeSnsSuccess,
-      timex_module: FakeTimex,
-      topic: "topic",
-      group: "group",
-      default_region: "region"
-    ]
-
-    assert AwsSns.publish(context.valid_event, opts) == :ok
-
-    assert_received :timex_now
-
-    assert_received [
-      :sns_success,
-      "{\"action\":\"TEST_EVENT\",\"group\":\"group\",\"id\":\"e8db4c36-f0b6-585a-a0df-0c388e87599b\",\"occurred_at\":\"2015-06-29T05:55:55Z\",\"payload\":{},\"version\":\"1.0.0\"}"
+      "{\"action\":\"EVENT_ACTION\",\"group\":\"EVENT_GROUP\",\"id\":\"af7eae51-f47e-4ddb-a274-33037e17df6e\",\"occurred_at\":\"2015-06-29T04:44:44Z\",\"payload\":{\"data\":\"data\"},\"version\":\"1.0.0\"}"
     ]
 
     assert_received [:ex_aws_success, _, _]
--- test/events/event_test.exs
+++ test/events/event_test.exs
@@ -12,7 +12,31 @@ defmodule ElixirTools.Events.EventTest do
   defmodule FakeAdapterSuccess do
     @behaviour ElixirTools.Events.Publisher
     @impl true
-    def publish(_, _ \\ []), do: :ok
+    def publish(event, _ \\ []) do
+      send(self(), {:publish_event, event})
+
+      :ok
+    end
+  end
+
+  defmodule FakeUuid do
+    use ElixirTools.ContractImpl, module: UUID
+
+    @impl true
+    def uuid5(uuid_seed_1, uuid_seed_2) do
+      send(self(), [:uuid5, uuid_seed_1, uuid_seed_2])
+      "55989ea4-d947-4f16-b8c8-e0e888facff4"
+    end
+  end
+
+  defmodule FakeTimex do
+    use ElixirTools.ContractImpl, module: Timex
+
+    @impl true
+    def now() do
+      send(self(), :timex_now)
+      Timex.to_datetime({{2015, 6, 29}, {5, 55, 55}}, "Etc/UTC")
+    end
   end
 
   setup do
@@ -24,65 +48,82 @@ defmodule ElixirTools.Events.EventTest do
     event_json_schema =
       "test/events/fixtures/json_schemas/json_schema.json" |> File.read!() |> Jason.decode!()
 
-    %{valid_event: valid_event, event_json_schema: event_json_schema}
+    opts = [{:uuid_module, FakeUuid}, {:timex_module, FakeTimex}, {:adapter, FakeAdapterSuccess}]
+
+    %{valid_event: valid_event, event_json_schema: event_json_schema, opts: opts}
   end
 
   describe "publish/2" do
     test "returns :ok when it's succesfully sent", context do
-      assert Event.publish(context.valid_event, FakeAdapterSuccess) == :ok
+      assert Event.publish(context.valid_event, context.opts) == :ok
+    end
+
+    test "sends expected event structure to adapter", context do
+      Event.publish(context.valid_event, context.opts)
+
+      expected_event = %{
+        action: "TEST_EVENT",
+        group: "EVENT_GROUP",
+        id: "55989ea4-d947-4f16-b8c8-e0e888facff4",
+        occurred_at: "2015-06-29T05:55:55Z",
+        payload: %{},
+        version: "1.0.0"
+      }
+
+      assert_received({:publish_event, ^expected_event})
     end
 
     test "returns error when adapter throws error", context do
-      assert Event.publish(context.valid_event, FakeAdapterError) ==
-               {:error, %RuntimeError{message: "ERROR"}}
+      opts = [{:uuid_module, FakeUuid}, {:timex_module, FakeTimex}, {:adapter, FakeAdapterError}]
+
+      assert Event.publish(context.valid_event, opts) == {:error, %RuntimeError{message: "ERROR"}}
     end
 
     test "returns error when version is not a string", context do
       event = %{context.valid_event | version: 1}
 
-      assert Event.publish(event, FakeAdapterSuccess) ==
-               {:error, "Expected a string with a version"}
+      assert Event.publish(event, context.opts) == {:error, "Expected a string with a version"}
     end
 
     test "returns error when version is not having major.minor.fix format", context do
       event = %{context.valid_event | version: "1.1"}
 
-      assert Event.publish(event, FakeAdapterSuccess) ==
+      assert Event.publish(event, context.opts) ==
                {:error, "Expected version with 3 dots, but received 1.1"}
     end
 
     test "returns error when version major is not an integer", context do
       event = %{context.valid_event | version: "1a.1.1"}
 
-      assert Event.publish(event, FakeAdapterSuccess) ==
+      assert Event.publish(event, context.opts) ==
                {:error, "Expected a number for the major, but received 1a.1.1"}
     end
 
     test "returns error when version minor is not an integer", context do
       event = %{context.valid_event | version: "1.1a.1"}
 
-      assert Event.publish(event, FakeAdapterSuccess) ==
+      assert Event.publish(event, context.opts) ==
                {:error, "Expected a number for the minor, but received 1.1a.1"}
     end
 
     test "returns error when version fix is not an integer", context do
       event = %{context.valid_event | version: "1.1.1a"}
 
-      assert Event.publish(event, FakeAdapterSuccess) ==
+      assert Event.publish(event, context.opts) ==
                {:error, "Expected a number for the fix, but received 1.1.1a"}
     end
 
     test "returns error when name does not contain an underscore", context do
       event = %{context.valid_event | name: "EVENT"}
 
-      assert Event.publish(event, FakeAdapterSuccess) ==
+      assert Event.publish(event, context.opts) ==
                {:error, "Expected an underscore in the event name, but got EVENT instead"}
     end
 
     test "returns error when name is not a string", context do
       event = %{context.valid_event | name: :EVENT}
 
-      assert Event.publish(event, FakeAdapterSuccess) ==
+      assert Event.publish(event, context.opts) ==
                {:error, "Expected a string as event name, but got :EVENT"}
     end
 
@@ -90,42 +131,41 @@ defmodule ElixirTools.Events.EventTest do
       Enum.map([[], false, nil, 3, 0, "string", 'binary', :atom], fn value ->
         event = %{context.valid_event | payload: value}
 
-        assert Event.publish(event, FakeAdapterSuccess) ==
-                 {:error, "Expected payload to be a map"}
+        assert Event.publish(event, context.opts) == {:error, "Expected payload to be a map"}
       end)
     end
 
     test "returns error when event_id_seed is not a UUID string", context do
       event = %{context.valid_event | event_id_seed: "not uuid string"}
 
-      assert Event.publish(event, FakeAdapterSuccess) ==
+      assert Event.publish(event, context.opts) ==
                {:error, "Expected a UUID string as event_id_seed, but got \"not uuid string\""}
     end
 
     test "returns error when event_id_seed_optional is not a string", context do
       event = %{context.valid_event | event_id_seed_optional: 123}
 
-      assert Event.publish(event, FakeAdapterSuccess) ==
+      assert Event.publish(event, context.opts) ==
                {:error, "Expected a string as event_id_seed_optional, but got 123"}
     end
 
     test "returns ok when event_id_seed_optional is string", context do
       event = %{context.valid_event | event_id_seed_optional: "i am string"}
 
-      assert Event.publish(event, FakeAdapterSuccess) == :ok
+      assert Event.publish(event, context.opts) == :ok
     end
 
     test "returns error when occurred_at is not a datetime", context do
       event = %{context.valid_event | occurred_at: "not a datetime"}
 
-      assert Event.publish(event, FakeAdapterSuccess) ==
+      assert Event.publish(event, context.opts) ==
                {:error, "Expected a DateTime as occurred_at, but got \"not a datetime\""}
     end
 
     test "returns ok when occurred_at is datetime", context do
       event = %{context.valid_event | occurred_at: Timex.now()}
 
-      assert Event.publish(event, FakeAdapterSuccess) == :ok
+      assert Event.publish(event, context.opts) == :ok
     end
   end
 
