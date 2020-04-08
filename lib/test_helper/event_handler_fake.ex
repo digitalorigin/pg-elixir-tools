803d95725c3323534611af2d704e5619bbe434fd
dabeat <garcia.narbona@gmail.com>
2020-04-08 15:10:00 +0200

    event fake module

--- lib/test_helper/event_handler_fake.ex
+++ lib/test_helper/event_handler_fake.ex
@@ -10,6 +10,12 @@ defmodule ElixirTools.TestHelper.EventHandlerFake do
   end
 
   @impl true
+  def publish(event, schema, _) do
+    send(self(), {:publish_event, [event, schema]})
+    :ok
+  end
+
+  @impl true
   def create(event_name, payload, event_id_seed) do
     send(self(), {:create_event, [event_name, payload, event_id_seed]})
     %Event{name: event_name, payload: payload, event_id_seed: event_id_seed}
