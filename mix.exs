3995a86ff3ad5e05553222f1b4468733c40829c0
Hylke Alons <halons@digitalorigin.com>
2020-06-09 11:49:24 +0200

    Bump to v0.21.3

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ElixirTools.MixProject do
   use Mix.Project
 
-  @version "0.21.2"
+  @version "0.21.3"
   @description "Tools used in Pagantis for making developing easier"
 
   def project do
